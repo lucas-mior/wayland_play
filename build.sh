7f58a1030e560f9111ada0ccf98d0bef630abeb4
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 16:52:22 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+# shellcheck disable=SC2086
 set -e
 
 if [ -n "$BASH_VERSION" ]; then
@@ -68,15 +69,19 @@ CFLAGS="$CFLAGS -Wno-unknown-pragmas"
 CFLAGS="$CFLAGS -Wno-char-subscripts"
 
 OS=$(uname -a)
-requested_cc=${CC:-}
+
 case "$target" in
-"debug"|"test"|"fast_feedback")
-    CC="${requested_cc:-tcc}"
+debug|test)
+    CC="${CC:-tcc}"
+    ;;
+fast_feedback)
+    CC="${CC:-clang}"
     ;;
 *)
-    CC="${requested_cc:-cc}"
+    CC="${CC:-cc}"
     ;;
 esac
+
 GNUSOURCE=
 
 if [ "$CC" = "clang" ]; then
