5453499da24da568efa781fad074ebb1657ef415
lucas-mior <lucas.mior@tutamail.com>
2026-09-01 12:53:23 -0300

    wip

--- build.sh
+++ build.sh
@@ -40,7 +40,6 @@ CPPFLAGS="$CPPFLAGS -Icbase"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
 case "$mode" in
 debug)
